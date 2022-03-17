ab7729c413a2411628bf59ec6ebbc4254bc5c411
ბაჩო ბექაური <81110694+200MB@users.noreply.github.com>
2022-03-17 23:26:29 +0400

    TvTitle and Celebrity parser added.

--- src/main/java/Api/Parser.java
+++ src/main/java/Api/Parser.java
@@ -1,10 +1,12 @@
 package Api;
 
+import Model.SearchCeleb;
 import Model.SearchTitle;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import javax.print.Doc;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
@@ -38,28 +40,73 @@ public class Parser {
         arrElements.add(writers);
 
 
-        return setInstance(arrElements,intance,ignore);
+        return setInstance(arrElements, intance, ignore);
     }
 
-    private static SearchTitle setInstance(ArrayList<Elements>arrElements,SearchTitle instance,Boolean ignore) {
+    private static SearchTitle setInstance(ArrayList<Elements> arrElements, SearchTitle instance, Boolean ignore) {
         int ind = 1;
         for (Elements i : arrElements) {
-            if (i.size() == 0 && ignore){
+            if (i.size() == 0 && ignore) {
                 return null;
             }
-            if (i.size() != 0) {
-                if (i.size() > 1 && ind == 3) {
+            if (ind == 3) {
+                if (i.size() > 1) {
                     instance.setAgeRating(i.get(1).text());
                     ind++;
                     continue;
+                } else {
+                    instance.setAgeRating("Undefined");
+                    ind++;
+                    continue;
                 }
-                instance.setWhat(ind, Arrays.asList(i.get(0).text()));
             }
-            if (i.size() == 1 && ignore && ind == 3){
-                return null;
+            if (i.size() != 0) {
+                instance.setWhat(ind, Arrays.asList(i.get(0).text()));
             }
             ind++;
         }
         return instance;
     }
+
+
+    protected static SearchCeleb parseToCeleb(Document doc, boolean ignore) {
+        SearchCeleb instance = new SearchCeleb();
+        Elements professions = doc.select("div[class=infobar] a[href]");
+        Elements birthYear = doc.select("time[datetime]");
+        Elements birthCountry = doc.select("div[id=name-born-info] a");
+        Elements filmography = doc.select("div[class=filmo-category-section] b a");
+        ArrayList<String> profs = new ArrayList<>();
+        LinkedHashSet<String> filmographyList = new LinkedHashSet<>();
+        ArrayList<Elements> arrElements = new ArrayList<>();
+        for (Element i : professions) {
+            profs.add(i.text());
+        }
+        for (Element i : filmography) {
+            filmographyList.add(i.text());
+        }
+        instance.setProfessions(profs);
+        instance.setFimlography(filmographyList);
+        arrElements.add(birthYear);
+        arrElements.add(birthCountry);
+
+
+        return setInstanceOfCeleb(instance, arrElements, ignore);
+    }
+
+    private static SearchCeleb setInstanceOfCeleb(SearchCeleb instance, ArrayList<Elements> arrElements, boolean ignore) {
+        int ind = 1;
+        for (Elements i : arrElements) {
+            if (i.size() == 0 && ignore) {
+                return null;
+            }
+            if (ind == 2) {
+                instance.setBirthCountry(i.get(2).text());
+                break;
+            }
+            instance.setWhat(ind, i.get(0).text());
+            ind++;
+        }
+        return instance;
+    }
 }
+
