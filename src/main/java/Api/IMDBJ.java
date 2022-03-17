b23971509f0b4d8d96d4f24824c17f6e6683403c
ბაჩო ბექაური <81110694+200MB@users.noreply.github.com>
2022-03-17 23:27:08 +0400

    Optimizations

--- src/main/java/Api/IMDBJ.java
+++ src/main/java/Api/IMDBJ.java
@@ -1,6 +1,8 @@
 package Api;
 
+import Model.SearchCeleb;
 import Model.SearchTitle;
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -8,27 +10,49 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 
 public class IMDBJ {
     private String link;
     private ArrayList<Document> linkArrList;
 
-    public ArrayList<SearchTitle> searchTitles(String name,int limit,Boolean ignore) throws IOException {
+    public ArrayList<SearchTitle> searchTitles(String name, int limit, Boolean ignore) throws IOException {
         link = SearchStrings.TITLE.formatted(name);
-        ArrayList<SearchTitle> tempList = new ArrayList<>(); getlinks(limit);
+        ArrayList<SearchTitle> tempList = new ArrayList<>();
+        getlinks(limit);
+        System.out.println("Parsing...");
         for (Document doc : linkArrList) {
-            tempList.add(Parser.parseToTitle(doc,ignore));
+            tempList.add(Parser.parseToTitle(doc, ignore));
         }
+        System.out.println("Ready!");
         return tempList;
 
     }
 
-    public void searchTvEpisodes(String name) throws IOException {
+    public ArrayList<SearchTitle> searchTvTitles(String name, int limit) throws IOException {
         link = SearchStrings.TVEP.formatted(name);
+        ArrayList<SearchTitle> tempList = new ArrayList<>();
+        getlinks(limit);
+        System.out.println("Parsing...");
+        for (Document doc : linkArrList) {
+            tempList.add(Parser.parseToTitle(doc, false));
+        }
+        System.out.println("Ready!");
+        return tempList;
     }
 
-    public void searchCelebs(String name) throws IOException {
+    public LinkedHashSet<SearchCeleb> searchCelebs(String name, int limit) throws IOException {
+        name = name.replace(' ', '+');
         link = SearchStrings.CELEBS.formatted(name);
+        LinkedHashSet<SearchCeleb> tempList = new LinkedHashSet<>();
+        getlinksForCelebs(limit);
+        System.out.println("Parsing...");
+        for (Document doc : linkArrList) {
+            tempList.add(Parser.parseToCeleb(doc, true));
+        }
+        System.out.println("Ready!");
+        tempList.remove(null);
+        return tempList;
     }
 
     public void searchByKeywords(String[] args) {
@@ -41,15 +65,54 @@ public class IMDBJ {
         Elements newsHeadlines = doc.select("table[class=findList] td[class=result_text] a"); //(div id)
         ArrayList<Document> linkList = new ArrayList<>();
         int index = 0;
+        System.out.println("Fetching...");
         for (Element element : newsHeadlines) {
-            if (index == limit){
+            if (index == limit) {
                 break;
             }
-            Document tempDoc = Jsoup.connect("https://www.imdb.com/%s".formatted(element.attr("href"))).get();
+            System.out.println("https://www.imdb.com%s".formatted(element.attr("href")));
+            Document tempDoc;
+            try {
+                tempDoc = Jsoup.connect("https://www.imdb.com%s".formatted(element.attr("href"))).get();
+            }
+            catch (HttpStatusException e){
+                continue;
+            }
             linkList.add(tempDoc);
             index++;
         }
         System.out.println("Done fetching!");
         linkArrList = linkList;
+    }
+
+    private void getlinksForCelebs(int limit) throws IOException {
+        Document doc = Jsoup.connect(link).get();
+        System.out.println(doc.title());
+        Elements newsHeadlines = doc.select("table[class=findList] td[class=result_text] a"); //(div id)
+        ArrayList<Document> linkList = new ArrayList<>();
+        int index = 0;
+        System.out.println("Fetching...");
+        for (int i = 0;i< newsHeadlines.size();i++) {
+            if (i % 2 != 0){
+                continue;
+            }
+            Element element = newsHeadlines.get(i);
+            if (index == limit) {
+                break;
+            }
+            System.out.println("https://www.imdb.com%s".formatted(element.attr("href")));
+            Document tempDoc;
+            try {
+                 tempDoc = Jsoup.connect("https://www.imdb.com%s".formatted(element.attr("href"))).get();
+            }
+            catch (HttpStatusException e){
+                continue;
+            }
+            linkList.add(tempDoc);
+            index++;
+        }
+        System.out.println("Done fetching!");
+        linkArrList = linkList;
+
     }
 }
