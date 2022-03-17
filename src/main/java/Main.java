e22cc09bcf12929f6b3b7b1510c090ce292b42cd
ბაჩო ბექაური <81110694+200MB@users.noreply.github.com>
2022-03-17 14:46:33 +0400

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,20 @@
 import Api.IMDBJ;
+import Model.SearchTitle;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
         IMDBJ api = new IMDBJ();
-        api.searchByKeywords(new String[]{"avengers","tony-stark"});
+        ArrayList<SearchTitle> list = api.searchTitles("spiderman", 5, true);
+        for (SearchTitle i : list) {
+            System.out.println(i);
+        }
     }
 }
