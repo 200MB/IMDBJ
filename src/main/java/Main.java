fef34468715d71ca3e9b5b3c08817c278bd51c6d
ბაჩო ბექაური <81110694+200MB@users.noreply.github.com>
2022-03-18 11:39:27 +0400

    Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import Api.IMDBJ;
 import Model.SearchCeleb;
 import Model.SearchTitle;
+import Model.SearchTvTitle;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -14,10 +15,10 @@ import java.util.LinkedHashSet;
 public class Main {
     public static void main(String[] args) throws IOException {
         IMDBJ api = new IMDBJ();
-//        ArrayList<SearchTitle> list = api.searchTitles("antman", 5,false);
-//        for (SearchTitle i : list) {
-//            System.out.println(i);
-//        }
+        ArrayList<SearchTvTitle> list = api.searchTvTitles("spiderman", 5);
+        for (SearchTvTitle i : list) {
+            System.out.println(i);
+        }
 //        LinkedHashSet<SearchCeleb> list = api.searchCelebs("", 25);
 //        for (SearchCeleb i : list) {
 //            System.out.println(i);
