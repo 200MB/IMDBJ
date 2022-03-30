ab50cbdb859813780e9f60f84e38b83d08bf6ba1
ბაჩო ბექაური <81110694+200MB@users.noreply.github.com>
2022-03-30 19:45:26 +0400

    Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,15 @@
 import Api.IMDBJ;
-import Model.SearchCeleb;
-import Model.SearchTitle;
 import Model.SearchTvTitle;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
 
 
 public class Main {
+    //TODO: IMDBJ has methods that have similar algorithms needs to be more generic and requires optimizations
     public static void main(String[] args) throws IOException {
         IMDBJ api = new IMDBJ();
-        ArrayList<SearchTvTitle> list = api.searchTvTitles("spiderman", 5);
-        for (SearchTvTitle i : list) {
-            System.out.println(i);
+        for (SearchTvTitle title : api.getMostPopularTvShows(5)){
+            System.out.println(title);
         }
-//        LinkedHashSet<SearchCeleb> list = api.searchCelebs("", 25);
-//        for (SearchCeleb i : list) {
-//            System.out.println(i);
-//        }
     }
 }
