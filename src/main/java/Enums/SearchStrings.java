1a92e1f4cc997da64b6cf49d957e633a94affc6d
ბაჩო ბექაური <81110694+200MB@users.noreply.github.com>
2022-03-30 19:47:22 +0400

    Moved to Enum package

---
+++ src/main/java/Enums/SearchStrings.java
@@ -0,0 +1,11 @@
+package Enums;
+
+public final class SearchStrings {
+    public static final String MOVIE = "https://www.imdb.com/find?q=%s&s=tt&ttype=ft&ref_=fn_ft";
+    public static final String TVEP = "https://www.imdb.com/find?q=%s&s=tt&ttype=tv&ref_=fn_tv";
+    public static final String CELEBS = "https://www.imdb.com/find?s=nm&q=%s&ref_=nv_sr_sm";
+    public static final String TOP250 = "https://www.imdb.com/chart/top/?ref_=nv_mv_250";
+    public static final String TOP250TV = "https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250";
+    public static final String MOST_POPULAR_MOVIES = "https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm";
+    public static final String MOST_POPULAR_TV_SHOWS = "https://www.imdb.com/chart/tvmeter/?ref_=nv_tvv_mptv";
+}
