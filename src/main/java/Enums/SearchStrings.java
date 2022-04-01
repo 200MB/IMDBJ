1e8a3003bdeb53097f0d7cd10059efaaa55dcfec
ბაჩო ბექაური <81110694+200MB@users.noreply.github.com>
2022-04-01 22:25:21 +0400

    added links for new scrapped pages

--- src/main/java/Enums/SearchStrings.java
+++ src/main/java/Enums/SearchStrings.java
@@ -8,4 +8,7 @@ public final class SearchStrings {
     public static final String TOP250TV = "https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250";
     public static final String MOST_POPULAR_MOVIES = "https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm";
     public static final String MOST_POPULAR_TV_SHOWS = "https://www.imdb.com/chart/tvmeter/?ref_=nv_tvv_mptv";
+    public static final String COMING_SOON = "https://www.imdb.com/movies-coming-soon/%s-%s/"; //year-month
+    public static final String MOVIES_BY_GENRE = "https://www.imdb.com/search/title/?genres=%s&title_type=feature&explore=genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=facfbd0c-6f3d-4c05-9348-22eebd58852e&pf_rd_r=HA2KFK9G96YQF0W7XMTP&pf_rd_s=center-6&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_mvpop_1";
+    public static final String TV_BY_GENRE = "https://www.imdb.com/search/title/?genres=%s&title_type=tv_series,mini_series&explore=genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=b4e1d6fb-9821-4c7d-ad14-31ed10854442&pf_rd_r=HA2KFK9G96YQF0W7XMTP&pf_rd_s=center-7&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_tvpop_1";
 }
