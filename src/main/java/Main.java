06c845bb65064b0e44c863d3d96068d78aecacbb
ბაჩო ბექაური <81110694+200MB@users.noreply.github.com>
2022-04-01 22:23:56 +0400

    BUG HUNTING AND CODE OPTIMIZATION IS A GO.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,22 @@
 import Api.IMDBJ;
+import Enums.Genre;
 import Model.SearchTvTitle;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Month;
+import java.time.MonthDay;
+import java.time.Year;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class Main {
     //TODO: IMDBJ has methods that have similar algorithms needs to be more generic and requires optimizations
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         IMDBJ api = new IMDBJ();
-        for (SearchTvTitle title : api.getMostPopularTvShows(5)){
-            System.out.println(title);
-        }
+        api.getTop50TvShowsByGenre(Genre.COMEDY,51).forEach(System.out::println);
     }
 }
