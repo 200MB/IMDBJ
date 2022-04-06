567b4a4ba7dd5e0f0f5183c458a2c9715802aa6c
ბაჩო ბექაური <81110694+200MB@users.noreply.github.com>
2022-04-06 21:54:41 +0400

    opt-s

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,19 @@
 import Api.IMDBJ;
-import Enums.Genre;
-import Model.SearchTvTitle;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Month;
-import java.time.MonthDay;
-import java.time.Year;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
+import java.util.jar.JarEntry;
 
 
 public class Main {
     //TODO: IMDBJ has methods that have similar algorithms needs to be more generic and requires optimizations
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args) throws IOException, ParseException, InterruptedException {
         IMDBJ api = new IMDBJ();
-        api.getTop50TvShowsByGenre(Genre.COMEDY,51).forEach(System.out::println);
+
     }
 }
