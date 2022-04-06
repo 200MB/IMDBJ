f542e4052904752b39b024573ba6f1c3888e45c5
ბაჩო ბექაური <81110694+200MB@users.noreply.github.com>
2022-04-06 21:53:51 +0400

    major optimization.

--- src/main/java/Api/Parser.java
+++ src/main/java/Api/Parser.java
@@ -13,110 +13,57 @@ import java.util.Collections;
 import java.util.LinkedHashSet;
 
 public class Parser {
-    protected static SearchMovie parseToMovie(Document doc, Boolean ignore) {
+    protected static SearchMovie parseToMovie(Document doc) {
         SearchMovie intance = new SearchMovie();
-        ArrayList<Elements> arrElements = new ArrayList<>();
-        Elements title = doc.select("h1[data-testid=hero-title-block__title]");
-        Elements year = doc.select("span[class=sc-52284603-2 iTRONr]");
+        Element title = doc.selectFirst("h1[data-testid=hero-title-block__title]");
+        Element year = doc.selectFirst("span[class=sc-52284603-2 iTRONr]");
         Elements ageRating = doc.select("span[class=sc-52284603-2 iTRONr]");
         Elements duration = doc.select("ul[class=ipc-inline-list ipc-inline-list--show-dividers sc-52284603-0 blbaZJ baseAlt] li");
-        Elements imdbRating = doc.select("div[class=sc-7ab21ed2-2 kYEdvH] span[class=sc-7ab21ed2-1 jGRxWM]");
-        Elements metaScore = doc.select("span[class=score-meta]");
-        Elements popularity = doc.select("div[class=sc-edc76a2-1 gopMqI]");
-        Elements director = doc.select("a[class=ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link]");
+        Element imdbRating = doc.selectFirst("div[class=sc-7ab21ed2-2 kYEdvH] span[class=sc-7ab21ed2-1 jGRxWM]");
+        Element metaScore = doc.selectFirst("span[class=score-meta]");
+        Element popularity = doc.selectFirst("div[class=sc-edc76a2-1 gopMqI]");
+        Element director = doc.selectFirst("a[class=ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link]");
         Elements writers = doc.select("ul[class=ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--inline ipc-metadata-list-item__list-content baseAlt] li");
         LinkedHashSet<String> tempList = new LinkedHashSet<>();
         for (Element element : writers) {
             tempList.add(element.text());
         }
         intance.setWriters(tempList);
-        arrElements.add(title);
-        arrElements.add(year);
-        arrElements.add(ageRating);
-        arrElements.add(duration);
-        arrElements.add(imdbRating);
-        arrElements.add(metaScore);
-        arrElements.add(popularity);
-        arrElements.add(director);
-        arrElements.add(writers);
+        intance.setTitle(title == null ? null : title.text());
+        intance.setYear(year == null ? null : year.text());
+        intance.setImdbRating(imdbRating == null ? null : imdbRating.text());
+        intance.setMetaScore(metaScore == null ? null : metaScore.text());
+        intance.setPopularity(popularity == null ? null : popularity.text());
+        intance.setDirector(director == null ? null : director.text());
+        intance.setAgeRating(ageRating == null ? null : ageRating.size() > 1 ? ageRating.get(1).text() : "UxR");
+        intance.setDuration(duration == null ? null : duration.size() > 2 ? duration.get(2).text() : "UxR");
+        if (!isMovie(doc)) {
+            try {
+                intance.setAgeRating(ageRating.get(1).text());
+            } catch (IndexOutOfBoundsException e) {
+                intance.setAgeRating(null);
+            }
+            try {
+                intance.setDuration(duration.get(3).text());
+            } catch (IndexOutOfBoundsException e) {
+                intance.setDuration(null);
+            }
+            return intance;
+        }
 
-        return setInstance(arrElements, intance, ignore);
+        return intance;
     }
 
     public static SearchTvTitle parseToTv(Document doc) {
-        SearchMovie ins = parseToMovie(doc,false);
-        return new SearchTvTitle(ins.getTitle(),ins.getYear(), ins.getAgeRating(), ins.getDuration(), ins.getImdbRating(),ins.getPopularity(),ins.getDirector(),ins.getWriters());
+        SearchMovie ins = parseToMovie(doc);
+        return new SearchTvTitle(ins.getTitle(), ins.getYear(), ins.getAgeRating(), ins.getDuration(), ins.getImdbRating(), ins.getPopularity(), ins.getDirector(), ins.getWriters());
     }
 
-    private static SearchMovie setInstance(ArrayList<Elements> arrElements, SearchMovie instance, Boolean ignore) {
-        int ind = 1;
-        for (Elements i : arrElements) {
-            int durationIndex = 2;
-            if (i.size() == 0 && ignore) {
-                return null;
-            }
-            if (ind == 4){
-                try{
-                    if (i.size() > 3){
-                        durationIndex = 3;
-                    }
-                    if (i.size() < 3){
-                        durationIndex = 1;
-                    }
-                    instance.setDuration(i.get(durationIndex).text());
-                    ind++;
-                    continue;
-                }
-                catch (IndexOutOfBoundsException e){
-                    instance.setDuration(null);
-                    ind++;
-                    continue;
-                }
-            }
-            if (ind == 3) {
-                if (i.size() > 1) {
-                    instance.setAgeRating(i.get(1).text());
-                    ind++;
-                    continue;
-                } else {
-                    instance.setAgeRating(null);
-                    ind++;
-                    continue;
-                }
-            }
-            if (i.size() != 0) {
-                instance.setWhat(ind, Arrays.asList(i.get(0).text()));
-            }
-            ind++;
+    public static Boolean isMovie(Document doc) {
+        if (doc.selectFirst("ul[class=ipc-inline-list ipc-inline-list--show-dividers sc-52284603-0 blbaZJ baseAlt] li") != null) {
+            return false;
         }
-
-        return instance;
-    }
-
-    private static SearchTvTitle setInstanceForTv(ArrayList<Elements> arrElements, SearchTvTitle instance, Boolean ignore) {
-        int ind = 1;
-        for (Elements i : arrElements) {
-            if (i.size() == 0 && ignore) {
-                return null;
-            }
-            if (ind == 3) {
-                if (i.size() > 1) {
-                    instance.setAgeRating(i.get(1).text());
-                    ind++;
-                    continue;
-                } else {
-                    instance.setAgeRating(null);
-                    ind++;
-                    continue;
-                }
-            }
-            if (i.size() != 0) {
-                instance.setWhat(ind, Collections.singletonList(i.get(0).text()));
-            }
-            ind++;
-        }
-
-        return instance;
+        return true;
     }
 
 
